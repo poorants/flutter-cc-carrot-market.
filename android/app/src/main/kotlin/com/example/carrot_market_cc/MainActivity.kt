6f3b7a0e4d80c3300a090913a0a033375b6a2c19
unknown <poorants.work@gmail.com>
2022-01-07 16:47:30 +0900

    테마 테스트 중

---
+++ android/app/src/main/kotlin/com/example/carrot_market_cc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.carrot_market_cc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:carrot_market_cc/pages/home.dart';
+import 'package:carrot_market_cc/provider/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const String title = 'Light & Dark Theme';
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+        builder: (context, _) {
+          final themeProvider = Provider.of<ThemeProvider>(context);
+
+          return MaterialApp(
+            title: title,
+            themeMode: themeProvider.themeMode,
+            theme: MyThemes.lightTheme,
+            darkTheme: MyThemes.darkTheme,
+            home: const Home(),
+          );
+        },
+      );
+}
