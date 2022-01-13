8a9a1a5ddd4212d4611bf9b5c5dd5e4d6445a805
unknown <poorants.work@gmail.com>
2022-01-13 17:20:47 +0900

    애니메이션 테스트

--- lib/pages/login/widgets/body.dart
+++ lib/pages/login/widgets/body.dart
@@ -6,13 +6,13 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    // final Size size = MediaQuery.of(context).size;
     return Background(
         child: SingleChildScrollView(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
+        children: const [
+          Text(
             'LOADING',
             style: TextStyle(
               color: Colors.black87,
@@ -20,23 +20,101 @@ class Body extends StatelessWidget {
               fontSize: 35,
             ),
           ),
-          const SizedBox(
-            height: 40,
-          ),
           SizedBox(
-              height: size.height * 0.3,
-              width: size.height * 0.3,
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Colors.black54,
-                  borderRadius: BorderRadius.circular(size.height * 0.3),
-                ),
-              )),
-          const SizedBox(
+            height: 60,
+          ),
+          AnimateImage(),
+          SizedBox(
             height: 40,
           ),
         ],
       ),
     ));
+  }
+}
+
+class AnimateImage extends StatefulWidget {
+  const AnimateImage({Key? key}) : super(key: key);
+
+  @override
+  _AnimateImageState createState() => _AnimateImageState();
+}
+
+class _AnimateImageState extends State<AnimateImage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  late final Animation<Offset> offsetAnimation;
+  late final Animation<BorderRadius> borderRadiusAnimation;
+  late final Animation<double> rotateAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    )..repeat(reverse: true);
+
+    offsetAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(0, 1),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    borderRadiusAnimation = Tween<BorderRadius>(
+      begin: BorderRadius.circular(23),
+      end: BorderRadius.circular(3),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    rotateAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  Widget _buildAnimation(BuildContext context, Widget? child) {
+    return RotationTransition(
+      turns: rotateAnimation,
+      child: SlideTransition(
+        position: offsetAnimation,
+        child: Container(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.black54,
+              borderRadius: borderRadiusAnimation.value,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      builder: _buildAnimation,
+      animation: _controller,
+    );
   }
 }
